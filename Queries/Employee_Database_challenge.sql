ff84dd2a27c72a259d4d0b83bd292a9323e7bade
Joshua Carter <joshuagcarter@gmail.com>
2022-04-22 14:02:56 -0500

    Adding Deliverable 1 and 2 files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,61 @@
+-- Deliverable 1
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO retirement_titles
+FROM employees as e
+JOIN titles as t ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT *
+FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+				first_name,
+				last_name,
+				title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT *
+FROM unique_titles;
+
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT *
+FROM retiring_titles;
+
+-- Deliverable 2
+-- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in 
+-- the mentorship program
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS DE
+	ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT *
+FROM mentorship_eligibility;
+
+
